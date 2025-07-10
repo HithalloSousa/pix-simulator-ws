cf62cf70086a2ea809166e09241465c03cb8c691
Hithallo Sousa Santos <hsantos.developer@gmail.com>
2025-07-10 18:45:27 -0300

    Add basedir setup to support test configuration

--- payments/pix.py
+++ payments/pix.py
@@ -5,7 +5,7 @@ class Pix:
     def __init__(self):
         pass
     
-    def create_payment(self):
+    def create_payment(self, base_dir=""):
         # Criação do pagamento na instituição financeira
         bank_payment_id = str(uuid.uuid4())
         
@@ -15,7 +15,7 @@ class Pix:
         # Criação do QR_Code
         img = qrcode.make(hash_payment)
         # Salva a imagem como arquivo PNG
-        img.save(f"static/img/qr_code_payment_{bank_payment_id}.png")
+        img.save(f"{base_dir}static/img/qr_code_payment_{bank_payment_id}.png")
         
         return {
             "bank_payment_id": bank_payment_id,
